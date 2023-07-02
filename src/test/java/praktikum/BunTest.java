5bb7d68330717768be5886ae302b2dc1b3c96191
Msilith <Akoilka@yandex.ru>
2023-07-02 20:29:01 +0300

    Диплом 1 часть

---
+++ src/test/java/praktikum/BunTest.java
@@ -0,0 +1,34 @@
+package praktikum;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class BunTest {
+
+    private final String TEST_NAME = "хлебушек";
+    private final float TEST_PRICE = 10.5f;
+    private Bun bun;
+
+    @Before
+    public void setUp() {
+
+        bun = new Bun(TEST_NAME, TEST_PRICE);
+
+    }
+
+    @Test
+    public void getNameReturnCorrectValue() {
+
+        String actual = bun.getName();
+        assertEquals("Ошибка в имени булочки", TEST_NAME, actual);
+
+    }
+
+    @Test
+    public void getPriceReturnCorrectValue() {
+        float actual = bun.getPrice();
+        assertEquals("Ошибка в цене булочки", TEST_PRICE, actual, 0);
+    }
+
+}
